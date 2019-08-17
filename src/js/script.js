2c80064cd375a9212cf2db77a2a93bb35228c17d
Dagmara Tomasik <Tomasik.Dagmara@outlook.com>
2019-08-17 16:27:29 +0200

    Add price update

--- src/js/script.js
+++ src/js/script.js
@@ -211,7 +211,7 @@
 
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       thisProduct.priceElem.innerHTML = thisProduct.price;
-      console.log(thisProduct.params);
+      //console.log(thisProduct.params);
     }
 
     initAmountWidget(){
@@ -290,7 +290,9 @@
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated',{
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
   }
@@ -300,10 +302,12 @@
       const thisCart = this;
 
       thisCart.products = [];
+
+      thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
       thisCart.getElements(element);
       thisCart.initActions();
 
-      console.log('new Cart:', thisCart);
+      //console.log('new Cart:', thisCart);
     }
 
     getElements(element){
@@ -314,6 +318,12 @@
       thisCart.dom.wrapper = element;
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
       thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+
+      thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
+
+        for(let key of thisCart.renderTotalsKeys){
+          thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
+        }
     }
 
     initActions(){
@@ -321,6 +331,10 @@
 
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
       thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive)
+      });
+
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
@@ -333,7 +347,33 @@
       thisCart.dom.productList.appendChild(generatedDOM);
 
       thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
-      console.log('thisCart:', thisCart);
+      //console.log('thisCart:', thisCart);
+
+      thisCart.update();
+    }
+
+    update(){
+      const thisCart = this;
+
+      thisCart.totalNumber = 0;
+      thisCart.subtotalPrice = 0;
+
+      for(let cartProduct of thisCart.products){
+        thisCart.subtotalPrice += cartProduct.price;
+        thisCart.totalNumber += cartProduct.amount;
+      }
+
+      thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
+
+      for(let key of thisCart.renderTotalsKeys){
+        for(let elem of thisCart.dom[key]){
+          elem.innerHTML = thisCart[key];
+        }
+      }
+
+      console.log('totalNumber:', thisCart.totalNumber);
+      console.log('subtotalPrice:', thisCart.subtotalPrice);
+      console.log('totalPrice:', thisCart.totalPrice);
     }
   }
 
@@ -351,8 +391,8 @@
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
 
-        console.log('new CartProduct:', thisCartProduct);
-        console.log('productData:', menuProduct);
+        //console.log('new CartProduct:', thisCartProduct);
+        //console.log('productData:', menuProduct);
     }
 
     getElements(element){

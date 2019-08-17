dbc6a49079eb59fd42801d4f0f277ec73b95e972
Dagmara Tomasik <Tomasik.Dagmara@outlook.com>
2019-08-17 16:57:41 +0200

    Add product remove

--- src/js/script.js
+++ src/js/script.js
@@ -336,6 +336,10 @@
       thisCart.dom.productList.addEventListener('updated', function(){
         thisCart.update();
       });
+
+      thisCart.dom.productList.addEventListener('remove', function(){
+        thisCart.remove(event.detail.cartProduct);
+      });
     }
 
     add(menuProduct){
@@ -375,6 +379,19 @@
       console.log('subtotalPrice:', thisCart.subtotalPrice);
       console.log('totalPrice:', thisCart.totalPrice);
     }
+
+    remove(cartProduct){
+      const thisCart = this;
+
+      const index = thisCart.products.indexOf(cartProduct);
+      console.log(index);
+
+      thisCart.products.splice(cartProduct, 1);
+
+      cartProduct.dom.wrapper.remove();
+
+      thisCart.update();
+    }
   }
 
   class CartProduct{
@@ -390,6 +407,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
         //console.log('new CartProduct:', thisCartProduct);
         //console.log('productData:', menuProduct);
@@ -416,6 +434,34 @@
       thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
 
       thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove(){
+
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+      console.log('remove');
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+    /*  thisCartProduct.dom.edit.addEventListener('click', function(event){
+        event.preventDefault();
+      }); */
+
+      thisCartProduct.dom.remove.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }
